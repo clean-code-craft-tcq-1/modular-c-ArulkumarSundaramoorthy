113364fa1d999096c60339ace0ae23d04282ec4d
Arul Kumar Sundaramoorthy <arulkumarmoorthy@gmail.com>
2021-02-23 17:47:44 +0530

    Update main.h

--- main.h
+++ main.h
@@ -23,5 +23,6 @@ void testPairToNumber(enum MajorColor major,enum MinorColor minor,int expectedPa
 int GetPairNumberFromColor(const ColorPair* colorPair);
 void testNumberToPair(int pairNumber,enum MajorColor expectedMajor,enum MinorColor expectedMinor);
 void ColorPairToString(const ColorPair* colorPair, char* buffer);
+void ColorCodingPrint(void);
 	
 #endif
